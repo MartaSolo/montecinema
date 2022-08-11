71d391195542d7caf9fc65bb8b97a7dd3611d973
MartaSolo <marciamarcia@o2.pl>
2022-08-11 12:29:10 +0200

    Delete basic structure

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,34 @@
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+// import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="header">Header</div>
+  <div class="hero">Hero</div>
+  <div class="soon">Coming soon</div>
+  <div class="screenings">Screenings</div>
+  <div class="contact">Contact</div>
+</template>
+
+<style scoped>
+.header {
+  border: 1px solid blue;
+}
+.hero {
+  height: 100px;
+  border: 1px solid red;
+}
+.soon {
+  height: 100px;
+  border: 1px solid orange;
+}
+.screenings {
+  height: 200px;
+  border: 1px solid violet;
+}
+.contact {
+  height: 50px;
+  border: 1px solid yellow;
+}
+</style>
