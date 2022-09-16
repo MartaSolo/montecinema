10b89da62253eeb8762257f535f45394ca2d06fc
MartaSolo <marciamarcia@o2.pl>
2022-09-16 15:12:35 +0200

    Add HaederLanguage to HeaderPrimary

---
+++ src/components/Header/HeaderLanguage.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+import BaseButton from "@/components/global/BaseButton.vue";
+
+const activeLang = ref("EN");
+
+const changeLang = (lang) => {
+  activeLang.value = lang;
+};
+</script>
+
+<template>
+  <div class="language__wrapper">
+    <BaseButton
+      v-if="activeLang === 'PL'"
+      class="language"
+      size="tiny"
+      colorTheme="dark-empty"
+      @click="changeLang('EN')"
+      >EN</BaseButton
+    >
+    <BaseButton
+      v-if="activeLang === 'EN'"
+      class="language"
+      size="tiny"
+      colorTheme="dark-empty"
+      @click="changeLang('PL')"
+      >PL</BaseButton
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.language__wrapper {
+  display: flex;
+  justify-content: flex-end;
+  @include mediumScreen {
+    padding-bottom: 8px;
+  }
+}
+.button.language {
+  font-size: 0.8rem;
+  padding: 3px 11px;
+  height: 21px;
+}
+</style>
