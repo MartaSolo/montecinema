3e8ea54f5603fde1137a518f13746ba2a440a239
Marta Solomacha <marta.solomacha@monterail.com>
2024-07-28 18:21:17 +0200

    Refactor BaseImage

--- src/components/Global/BaseImage.vue
+++ src/components/Global/BaseImage.vue
@@ -1,17 +1,33 @@
 <script>
 import { defineComponent } from "vue";
+import ImagePlaceholder from "@/assets/images/star_wars.jpg";
 
 export default defineComponent({
-  name: "BaseImage",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      imagePlaceholder: ImagePlaceholder,
+    };
+  },
+  methods: {
+    handleError(e) {
+      e.target.src = this.imagePlaceholder;
+    },
+  },
 });
 </script>
 
-<template><img class="image" /></template>
+<template>
+  <img :src="src" :alt="alt" @error="handleError" />
+</template>
 
-<style lang="scss" scoped>
-.image {
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-}
-</style>
+<style lang="scss" scoped></style>
